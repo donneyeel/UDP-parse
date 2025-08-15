f4464441c69fd69a47643c60325fd969df198095
Daniil Vassilyev <daniilvas2003@gmail.com>
2025-08-15 15:56:57 +0300

    TPS is being plotted now

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
+np.set_printoptions(suppress=True)
 
-filename = "A-host.csv"
+filename1 = "A-host.csv"
+filename2 = "A-host_transactions.bin"
 
 udp_tx = []
 udp_rx = []
 timestamps = []
 
-with open(filename, "r") as f:
+binary_timesamps = np.fromfile(filename2, dtype=np.uint32) / 1_000_000
+binary_indices = np.arange(1, len(binary_timesamps)+1)
+transactions_per_second = binary_indices / binary_timesamps
+
+print("Transactions per second:", transactions_per_second)
+
+with open(filename1, "r") as f:
     reader = csv.DictReader(f)
     for row in reader:
         udp_tx.append(int(row["udp_tx"]))
         udp_rx.append(int(row["udp_rx"]))
-        timestamps.append(int(row["time_stamp"]))
+        timestamps.append(int(row["time_stamp"]) / 1_000_000)
+        
+fig, ax1 = plt.subplots(figsize=(10, 6))
 
-plt.figure(figsize=(10, 6))
-plt.plot(timestamps, udp_tx, label="udp_tx (bytes)", marker='o')
-plt.plot(timestamps, udp_rx, label="udp_rx (bytes)", marker='x')
+ax1.plot(timestamps, udp_tx, label="udp_tx (bytes)", marker='o', color='b')
+ax1.plot(timestamps, udp_rx, label="udp_rx (bytes)", marker='x', color='g')
+ax1.set_xlabel("Time (seconds)")
+ax1.set_ylabel("Bytes")
+ax1.legend(loc="upper left")
+ax1.grid(True)
 
-plt.xlabel("Time (microseconds)")
-plt.ylabel("Bytes")
-plt.title("UDP Transmission and Reception Over Time")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+ax2 = ax1.twinx()
+ax2.plot(binary_timesamps, transactions_per_second, label="Transactions per Second", marker='x', color='r')
+ax2.set_ylabel("TPS")
+ax2.legend(loc="upper right")
+
+plt.title("UDP Transmission, Reception, and Transactions per Second")
 plt.show()
